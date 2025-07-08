a5b54464494ebbda1f12d71266dc595eccafef87
montebruce <montebruce@proton.me>
2025-07-07 19:40:02 -0500

    updating to see if files can be found

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse, FileResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 import os
 
 app = FastAPI()
 
-# Mount static files (for style.css, images, and index.html)
+# Mount static files (for style.css, images, etc.)
 app.mount("/static", StaticFiles(directory=os.path.dirname(os.path.abspath(__file__))), name="static")
 
+templates = Jinja2Templates(directory=os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates"))
+
 @app.get("/", response_class=HTMLResponse)
-def read_root():
-    return FileResponse(os.path.join(os.path.dirname(os.path.abspath(__file__)), "static", "index.html")) 
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request}) 
